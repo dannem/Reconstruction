6880829436e4600b4a8a162c274461d17ce8b3e2
Dan Nemrodov <dannem@gmail.com>
2016-03-17 15:51:27 -0400

    update

--- IdealObserverL2.m
+++ IdealObserverL2.m
@@ -1,3 +1,6 @@
+ims_pca=ims2mat('/Users/dannem/Dropbox/CompNeuroLab/EEG Reconstruction/Stimuli/Final_PCA_Stimuli','tif');
+ims_orig=ims2mat('/Users/dannem/Dropbox/CompNeuroLab/EEG Reconstruction/Stimuli/FamiliarFamousFaces','tif');
+ims=ims_orig;
 maxVal=length(ims)/2;
 vecInd=nchoosek(1:maxVal,2);
 outputVal=zeros(maxVal);
